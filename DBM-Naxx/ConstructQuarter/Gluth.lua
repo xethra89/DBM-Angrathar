5fde986f2165f7f66b27eb9cfad3f460eb19e793
xethra89 <36385875+xethra89@users.noreply.github.com>
2018-03-06 19:29:08 -0500

    Add files via upload

---
+++ DBM-Naxx/ConstructQuarter/Gluth.lua
@@ -0,0 +1,37 @@
+local mod	= DBM:NewMod("Gluth", "DBM-Naxx", 2)
+local L		= mod:GetLocalizedStrings()
+
+mod:SetRevision(("$Revision: 2869 $"):sub(12, -3))
+mod:SetCreatureID(15932)
+
+mod:RegisterCombat("combat")
+
+mod:EnableModel()
+
+mod:RegisterEvents(
+	"SPELL_DAMAGE"
+)
+
+
+local warnDecimateSoon	= mod:NewSoonAnnounce(54426, 2)
+local warnDecimateNow	= mod:NewSpellAnnounce(54426, 3)
+
+local timerDecimate		= mod:NewCDTimer(105, 54426)
+
+
+function mod:OnCombatStart(delay)
+	timerDecimate:Start(105 - delay)
+	warnDecimateSoon:Schedule(95 - delay)
+end
+
+local decimateSpam = 0
+function mod:SPELL_DAMAGE(args)
+	if args:IsSpellID(28375) and (GetTime() - decimateSpam) > 20 then
+		decimateSpam = GetTime()
+		warnDecimateNow:Show()
+		timerDecimate:Start()
+		warnDecimateSoon:Schedule(96)
+	end
+end
+
+
