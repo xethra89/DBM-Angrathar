cf64044e73d42ed182a8416f9d8c4763c5ff1ea6
xethra89 <36385875+xethra89@users.noreply.github.com>
2018-03-11 22:54:48 -0400

    Update Sapphiron.lua
    Stomp still breaking Addon, removed again for now.

--- DBM-Naxx/FrostwyrmLair/Sapphiron.lua
+++ DBM-Naxx/FrostwyrmLair/Sapphiron.lua
@@ -22,7 +22,7 @@ local warnAirPhaseNow	= mod:NewAnnounce("WarningAirPhaseNow", 4, "Interface\\Add
 local warnLanded		= mod:NewAnnounce("WarningLanded", 4, "Interface\\AddOns\\DBM-Core\\textures\\CryptFiendBurrow.blp")
 
 local warnDeepBreath	= mod:NewSpecialWarning("WarningDeepBreath")
-local warnStomp			= mod:NewStompAnnounce(45185, 2)
+--local warnStomp			= mod:NewStompAnnounce(45185, 2)
 
 mod:AddBoolOption("WarningIceblock", true, "announce")
 
@@ -31,7 +31,7 @@ local timerAirPhase		= mod:NewTimer(66, "TimerAir", "Interface\\AddOns\\DBM-Core
 local timerLanding		= mod:NewTimer(28.5, "TimerLanding", "Interface\\AddOns\\DBM-Core\\textures\\CryptFiendBurrow.blp")
 local timerIceBlast		= mod:NewTimer(9.3, "TimerIceBlast", 15876)
 local enrageTimer		= mod:NewBerserkTimer(600)
-local timerStomp		= mod:NewCDTimer(10, 45185)
+--local timerStomp		= mod:NewCDTimer(10, 45185)
 local noTargetTime = 0
 local isFlying = false
 
@@ -55,9 +55,9 @@ function mod:SPELL_CAST_SUCCESS(args)
 		warnDrainLifeNow:Show()
 		warnDrainLifeSoon:Schedule(18.5)
 		timerDrainLife:Start()
-	elseif args:IsSpellID(45185) then
-		warnStomp:Show()
-		timerStomp:Start()
+--	elseif args:IsSpellID(45185) then
+--		warnStomp:Show()
+--		timerStomp:Start()
 	end
 end
 
